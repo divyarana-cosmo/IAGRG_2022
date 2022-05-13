81f4f4a52d33f0cee2c822a4cc865fa5749fb44c
menobista <divyar@iucaa.in>
2022-05-13 13:22:07 +0530

    test

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,9 @@ release = '2022'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ["nbsphinx"]
+#extensions = [
+#]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
