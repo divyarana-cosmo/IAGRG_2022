78f2f904f38b3aa4dc61d84ad53b2706083e06c0
menobista <divyar@iucaa.in>
2022-05-16 11:32:37 +0530

    updated

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = '2022'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["nbsphinx"]
+extensions = ['nbsphinx', 'sphinx_rtd_theme']
 #extensions = [
 #]
 
