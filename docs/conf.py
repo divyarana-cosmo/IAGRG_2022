d31bbd1b07846aa1b5e2af12ba8508e06a899f6b
menobista <divyar@iucaa.in>
2022-05-27 06:42:37 +0530

    updated

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Weak Lensing'
-copyright = '2022, Divya Rana'
-author = 'Divya Rana'
+copyright = '2022, Divya Rana and Navin Chaurasiya'
+author = 'Divya Rana and Navin Chaurasiya'
 
 # The full version, including alpha/beta/rc tags
 release = '2022'
